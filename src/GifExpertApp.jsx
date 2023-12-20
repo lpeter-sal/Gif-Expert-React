33432908839b754970c341e03cf050786f067e19
Pedro Salvador <lp.salvador@info.uas.edu.mx>
2023-12-20 00:28:46 -0700

    Primer Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState([]);
+
+    const onAddCategory = (newCategory) => {
+
+        if(categories.includes(newCategory)) return;
+
+        // setCategories(categories.concat('Valorant'));
+        // setCategories([...categories, 'Valorant']);
+        setCategories([newCategory, ...categories]);
+    }
+
+
+    const ResetCategory = () => {
+        setCategories([]);
+    }
+
+  return (
+    <>
+
+        <h1>GifExpertApp</h1>
+
+
+        <AddCategory 
+            onNewCategory={ (event) => onAddCategory(event)}
+        />
+
+        <button className='btn primary' onClick={ResetCategory}>Reset</button>
+
+        { 
+            categories.map( (category) => (
+                <GifGrid key={category} 
+                category={category} />
+            ))
+        }
+
+
+
+    </>
+  )
+}
